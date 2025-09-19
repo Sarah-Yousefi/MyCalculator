03c5385addb28cf812186dafdbbace338264087a
Sarah <srh.yousefi1379@gmail.com>
2025-09-19 23:34:21 +0330

    Final Calculator

--- app/src/main/java/com/mycalculator/Expression.kt
+++ app/src/main/java/com/mycalculator/Expression.kt
@@ -1,10 +1,13 @@
 package com.mycalculator
 
 import android.R
+import android.util.Log
 import java.util.Stack
+import kotlin.math.pow
 
 class Expression(var infixExpress: MutableList<String>) {
-    private fun infixToPostfix(): String {
+    var postFix: String=""
+    private fun infixToPostfix() {
         var result = ""
         val stack = Stack<String>()
         for (element in infixExpress){
@@ -19,22 +22,26 @@ class Expression(var infixExpress: MutableList<String>) {
                 if (stack.isNotEmpty()) {
                     stack.pop()
                 }
-            } else {
+            }
+            else {
                 while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(element)) {
                     result += "${stack.pop()}"
                 }
+            stack.push(element)
 
             }
         }
         while (stack.isNotEmpty()) {
             result += "${stack.pop()}"
         }
-        return result
+        postFix=result
     }
 
     private fun precedence(operator : String): Int {
         return when (operator) {
-            "*", "/" -> 2
+            "%" -> 4
+            "^" -> 3
+            "×", "÷" -> 2
             "+", "-" -> 1
             else -> -1
 
@@ -42,7 +49,8 @@ class Expression(var infixExpress: MutableList<String>) {
     }
 
 
-    fun evaluateExpression(postFix: String): Number {
+    fun evaluateExpression(): Number {
+        infixToPostfix()
         val stack= Stack <Double>()
         var i=0
         while (i< postFix.length){
@@ -60,12 +68,17 @@ class Expression(var infixExpress: MutableList<String>) {
             }
             else{
                 val x= stack.pop()
-                val y= stack.pop()
+                var y =1.0
+                if (stack.isNotEmpty()){
+                    y= stack.pop()
+                }
                 when(postFix[i]){
-                    '*' -> stack.push(x*y)
-                    '/' -> stack.push(x/y)
+                    '×' -> stack.push(x*y)
+                    '÷' -> stack.push(y/x)
                     '+' -> stack.push(x+y)
-                    '-' -> stack.push(x-y)
+                    '-' -> stack.push(y-x)
+                    '%' -> if (x<1) stack.push(x*100) else stack.push(x/100)
+                    '^' -> stack.push(y.pow(x))
                 }
             }
             i++
